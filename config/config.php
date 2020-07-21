d497bf25d06b215721ba032f3f00fe79f7d7eeae
StenQuidelleur <sten.quidelleur@outlook.fr>
2020-07-21 17:41:24 +0200

    config scalingo

--- config/config.php
+++ config/config.php
@@ -8,13 +8,9 @@
  *
  * @link     https://github.com/WildCodeSchool/simple-mvc
  */
-
-
 define('APP_DEV', true);
-
 //Model (for connexion data, see unversionned db.php)
 //VIew
-define('APP_VIEW_PATH', __DIR__ . '/../src/View/');
-define('APP_CACHE_PATH', __DIR__ . '/../temp/cache/');
-
+define('APP_VIEW_PATH', 'src/View/');
+define('APP_CACHE_PATH', 'temp/cache/');
 define('HOME_PAGE', 'home/index');
---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file define BD access infos.
+ *
+ * PHP version 7
+ *
+ * @category  PHP
+ * @package   PHP_SimpleMVC
+ * @author    WCS <contact@wildcodeschool.fr>
+ * @license  --
+ * @link     https://github.com/WildCodeSchool/simple-mvc
+ *
+ */
+
+define('APP_DB_USER', 'kitty_expres_2444');
+define('APP_DB_PWD', 'c01fzYVA3OUU6rWfUt50');
+define('APP_DB_HOST', '0c35bf02-aea7-43d7-9fb1-a738d02f49ec.kitty-expres-2444.mysql.dbs.scalingo.com:34057/kitty_expres_2444?useSSL=true&verifyServerCertificate=false');
+define('APP_DB_NAME', 'kitty_expres_2444');
